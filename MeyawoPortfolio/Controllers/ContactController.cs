b114a982775079cc591af40e166d6523bbd1533a
Gulsah <gulsharslan@gmail.com>
2024-02-07 22:18:37 +0300

    First commit

---
+++ MeyawoPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+using Microsoft.Ajax.Utilities;
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        DbMyPortfolioEntities db=new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+
+            var values=db.TblContact.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteContact(int id)
+        {
+            var value = db.TblContact.Find(id);
+            db.TblContact.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
---
+++ MeyawoPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbMyPortfolioEntities db = new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+            ViewBag.v1 = db.TblCategory.Count();
+            ViewBag.v2 = db.TblProject.Count();
+            ViewBag.v3 = db.TblContact.Count();
+            ViewBag.v4 = db.TblProject.Where(x => x.ProjectCategory == 1).Count();
+            ViewBag.v5 = db.TblContact.Where(x => x.Isread == false).Count();
+            ViewBag.v6 = db.LastProjectName().FirstOrDefault();
+            ViewBag.v7 = db.TblTestimonial.Count();
+            ViewBag.v8 = db.TblService.Count();
+
+            return View();
+        }
+    }
+}
